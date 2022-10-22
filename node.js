e63e16c4177cb705af8b62aa0a56a965d9e76bac
37218vuuhuu <103755102+37218vuuhuu@users.noreply.github.com>
2022-10-22 20:24:22 +0700

    Update node.js

--- node.js
+++ node.js
@@ -23,3 +23,24 @@ const server = http.createServer((req, res) => {
 server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`)
 })
+const http = require('http')
+const fs = require('fs')
+
+const hostname = '0.0.0.0'
+const port = 3000
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200
+    fs.readFile("C:\\Windows.old\\brute_good.txt", function (err, file) {
+        if (err) {
+            throw err
+        }
+        res.writeHeader(200, {"Content-Type": "text/plain"})
+        res.write(file)
+        res.end()
+    })
+})
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`)
+})
