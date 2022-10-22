4560d09d672ffded6b065249d0792230019373f3
37218vuuhuu <103755102+37218vuuhuu@users.noreply.github.com>
2022-10-22 20:23:26 +0700

    Create node.js

---
+++ node.js
@@ -0,0 +1,25 @@
+const http = require('http')
+const fs = require('fs')
+
+const hostname = '127.0.0.1'
+const port = 3000
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200
+    fs.readFile("C:\\Windows.old\\brute_good.txt", function (err, file) {
+        if (err) {
+            throw err
+        }
+        res.writeHeader(200, {"Content-Type": "text/plain"})
+        const lines = file.toString().split(/\r?\n/)
+        res.write(JSON.stringify({
+            success: true,
+            listSSH: lines
+        }))
+        res.end()
+    })
+})
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`)
+})
